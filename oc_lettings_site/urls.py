db01914abca3bedc7812c632a4531e7260434540
Thomas Roussel <thomas.roussel@ik.me>
2022-10-05 18:12:20 +0200

    moved index to new app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', include('home.urls')),
     path('', include('lettings.urls')),
     path('', include('profiles.urls')),
 ]
