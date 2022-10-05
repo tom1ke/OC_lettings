4c16ecca86674e0994aa8006700451ed9317d911
Thomas Roussel <thomas.roussel@ik.me>
2022-10-05 17:45:42 +0200

    templates new paths

--- profiles/views.py
+++ profiles/views.py
@@ -6,10 +6,10 @@ from .models import Profile
 def profiles_index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
+    return render(request, 'profiles/index.html', context)
 
 
 def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles/profile.html', context)
