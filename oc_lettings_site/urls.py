14d267c9c0e08583de3eb6d7ca098507bf4d6e92
Thomas Roussel <thomas.roussel@ik.me>
2022-10-20 20:12:10 +0200

    deleted sentry test

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,14 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-def trigger_error(request):
-    return 1 / 0
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('', include('lettings.urls')),
     path('', include('profiles.urls')),
-    path('sentry-debug/', trigger_error),
 ]
